8ce4fb089104b4fcc80107e17e916719e015b522
张刚敏 <490410638@qq.com>
2016-07-04 15:16:55 +0800

    分支 textView

--- app/src/main/java/com/gm/zhanggangmin/myapplication/MainActivity.java
+++ app/src/main/java/com/gm/zhanggangmin/myapplication/MainActivity.java
@@ -2,14 +2,24 @@ package com.gm.zhanggangmin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"2",Toast.LENGTH_LONG).show();
+
+        textView = (TextView) findViewById(R.id.txt_test);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "2", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
