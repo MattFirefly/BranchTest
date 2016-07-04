29d01667bab8888708fd2da58b2a294f319b4c7a
张刚敏 <490410638@qq.com>
2016-07-04 15:45:31 +0800

    主分支修改

--- app/src/main/java/com/gm/zhanggangmin/myapplication/MainActivity.java
+++ app/src/main/java/com/gm/zhanggangmin/myapplication/MainActivity.java
@@ -2,14 +2,24 @@ package com.gm.zhanggangmin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"!",Toast.LENGTH_LONG).show();
+
+        btn_test = (Button) findViewById(R.id.btn_test);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
